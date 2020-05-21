6fb94f95105550eaae1612cb0c76c84cdbcd0342
Kyle Harrison <redactedprofile@gmail.com>
2020-05-21 11:13:58 -0700

    formatting

--- deps.ts
+++ deps.ts
@@ -1,3 +1,7 @@
-export { assert, assertEquals, assertStrContains, } from "https://deno.land/std/testing/asserts.ts";
-export { serve } from 'https://deno.land/std/http/server.ts';
+export {
+  assert,
+  assertEquals,
+  assertStrContains,
+} from "https://deno.land/std/testing/asserts.ts";
+export { serve } from "https://deno.land/std/http/server.ts";
 export { md5 } from "https://deno.land/x/md5/mod.ts";
--- echo.ts
+++ echo.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "./deps.ts";
-import { Query } from './src/query.ts';
-import { Storage } from './src/storage.ts';
+import { Query } from "./src/query.ts";
+import { Storage } from "./src/storage.ts";
 
 const env = Deno.env.toObject();
 const PORT = env.PORT || 8200;
@@ -11,52 +11,49 @@ const decoder = new TextDecoder();
 
 const connections: Deno.Conn[] = [];
 
-const binaryListener = Deno.listen({port: 8200});
+const binaryListener = Deno.listen({ port: 8200 });
 
 console.log("Initializing Clean Storage");
 Storage.getInstance();
 
-
 console.log("Listening on 8200");
 
 for await (const conn of binaryListener) {
-    connections.push(conn);
-    await conn.write(encoder.encode("Welcome to Echo\r\n"))
-    handle_connection(conn);
+  connections.push(conn);
+  await conn.write(encoder.encode("Welcome to Echo\r\n"));
+  handle_connection(conn);
 }
 
 async function handle_connection(connection: Deno.Conn) {
-    let buffer = new Uint8Array(1024);
-    let work = true;
-    while(work) {
-        const count = await connection.read(buffer);
-        if(!count) {
-            // connection closed
-            const index = connections.indexOf(connection);
-            connections.splice(index, 1);
-            break;
-        } else {
-            // message received 
-            
-            for (const current_connection of connections) {
-                if(current_connection === connection) {
+  let buffer = new Uint8Array(1024);
+  let work = true;
+  while (work) {
+    const count = await connection.read(buffer);
+    if (!count) {
+      // connection closed
+      const index = connections.indexOf(connection);
+      connections.splice(index, 1);
+      break;
+    } else {
+      // message received
 
-                    console.log("message received");
-                    let message = buffer.subarray(0, count);
-                    let query = decoder.decode(message).toString().trim();
+      for (const current_connection of connections) {
+        if (current_connection === connection) {
+          console.log("message received");
+          let message = buffer.subarray(0, count);
+          let query = decoder.decode(message).toString().trim();
 
-                    console.log("Query received:", query);
-                    console.log(query);
+          console.log("Query received:", query);
+          console.log(query);
 
-                    const _query = new Query({
-                        query: query,
-                        connection: current_connection
-                    });
+          const _query = new Query({
+            query: query,
+            connection: current_connection,
+          });
 
-                    _query.handle_query();
-
-                }
-            }
+          _query.handle_query();
         }
+      }
     }
+  }
 }
