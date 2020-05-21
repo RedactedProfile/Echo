90d72af3b12a442c4d9067a6528559dadfbe76ec
Kyle Harrison <redactedprofile@gmail.com>
2020-05-21 01:49:59 -0700

    enhancing the command suites capabilities by passing in the connection object

--- query.ts
+++ query.ts
@@ -1,3 +1,7 @@
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
 export 
 enum Command {
     CHK,  // Exists / Checks
@@ -13,6 +17,7 @@ enum Command {
 export 
 interface IQueryConstructProps {
     query: string;
+    connection: Deno.Conn;
 };
 
 export 
@@ -21,33 +26,104 @@ type ExecutableCommand = {
 }
 
 export 
-class NukeCommand implements ExecutableCommand {
-    // Nuke is a command that doesn't take any arguments, no constructor is needed (at this time)
+class BaseCommand implements ExecutableCommand {
 
+    constructor(protected connection:Deno.Conn) {
+
+    }
+
+    async out(message:string) {
+        await this.connection.write(encoder.encode(`${message}\r\n`))
+    }
+
+    sanitize_value(_val:string) {
+        // function that strips off surrounding double quotes and removes any slashes found within
+        return _val;
+    }
+
+    execute():any {
+        console.log("Not Implemented");
+    }
+}
+
+export 
+class NukeCommand extends BaseCommand {
+    // Nuke is a command that doesn't take any arguments, no constructor is needed (at this time)
+    constructor(connection:Deno.Conn) {
+        super(connection)
+    }
     execute():any {
         console.log("Nuking this place to glass");
+        this.out("Nuked this place to glass");
     }
 }
 
 export 
-class KeyCommand implements ExecutableCommand {
-    constructor(private query:string) {
-
+class KeyCommand extends BaseCommand {
+    constructor(private query:string,connection:Deno.Conn) {
+        super(connection);
     }
 
     execute():any {
-        console.log("Looking for key names with this fuzzy match", this.query);
+        const _val = this.sanitize_value(this.query);
+        console.log("Looking for key names with this fuzzy match", _val);
     }
 }
 
 export 
-class FindCommand implements ExecutableCommand {
-    constructor(private query:string) {
-
+class FindCommand extends BaseCommand {
+    constructor(private query:string, connection:Deno.Conn) {
+        super(connection);
     }
 
     execute():any {
-        console.log("Looking for keys with this value somewhere", this.query);
+        const _val = this.sanitize_value(this.query);
+        console.log("Looking for keys with this value somewhere", _val);
+    }
+}
+
+export 
+class SetCommand extends BaseCommand {
+    constructor(private key:string, private value:string, private extras:Array<string>, connection:Deno.Conn) {
+        super(connection);
+    }
+
+    execute():any {
+        const _val = this.sanitize_value(this.value);
+        console.log("Setting key:", this.key, "with value:", _val, "and maybe", this.extras);
+    }
+}
+
+export 
+class GetCommand extends BaseCommand {
+    constructor(private key:string, connection:Deno.Conn) {
+        super(connection);
+    }
+
+    execute():any {
+        console.log("Retriving:", this.key, "'s value if any");
+    }
+}
+
+export 
+class ExistsCommand extends BaseCommand {
+    constructor(private key:string, connection:Deno.Conn) {
+        super(connection);
+    }
+
+    execute():any {
+        console.log("Determining if ", this.key, " exists");
+    }
+}
+
+export 
+class DeleteCommand extends BaseCommand {
+    constructor(private key:string, connection:Deno.Conn) {
+        super(connection);
+    }
+
+    execute():any {
+        console.log("Deleting key", this.key);
     }
 }
 
@@ -63,6 +139,8 @@ class Query {
     */
 
     original_query!: string;
+    original_pieces!:Array<string>;
+    conn!:Deno.Conn;
     
     // command!:Command;
     // key!:string;
@@ -76,6 +154,8 @@ class Query {
         private value:string,
         private extras:Array<string> 
     ) {
+        this.conn = props.connection;
+        
         if(props.query) {
             this.parse(props.query);
         }
@@ -87,9 +167,12 @@ class Query {
         // STEP 1: detect command, we can split by space and get the first element as the supplied command buffer
 
         // split the query up via a this regex pattern, lovingly found here: https://stackoverflow.com/a/9584469
-        const re_query_splitter = /\s+(?=((\\[\\"]|[^\\"])*"(\\[\\"]|[^\\"])*")*(\\[\\"]|[^\\"])*$)/;
+        const re_query_splitter = /\s+(?=((\\[\\"]|[^\\"])*"(\\[\\"]|[^\\"])*")*(\\[\\"]|[^\\"])*$)/g;
         const separator_string = '|#!|';
-        const q_pieces = query.replace(re_query_splitter, separator_string).split(separator_string);
+        const q_pieces = query.replace(re_query_splitter, separator_string)
+                              .split(separator_string);
+
+        this.original_pieces = Object.assign([], q_pieces); // informational storage
 
         const q_cmd = q_pieces.shift()?.toUpperCase();
 
@@ -137,22 +220,35 @@ class Query {
         if(q_pieces.length) {
             this.extras = q_pieces; }
 
-        console.log(this);
-
         return this.handle_query();
     }
 
     handle_query() {
+        
+        console.log(this);
+
         let cmd:ExecutableCommand;
         switch(this.command) {
+            case Command.CHK:
+                cmd = new ExistsCommand(this.key, this.conn);
+                break;
+            case Command.GET: 
+                cmd = new GetCommand(this.key, this.conn);
+                break;
+            case Command.SET:
+                cmd = new SetCommand(this.key, this.value, this.extras, this.conn);
+                break;
             case Command.NUK:
-                cmd = new NukeCommand();
+                cmd = new NukeCommand(this.conn);
                 break;
             case Command.KEY:
-                cmd = new KeyCommand(this.value);
+                cmd = new KeyCommand(this.value, this.conn);
                 break;
             case Command.FND:
-                cmd = new FindCommand(this.value);
+                cmd = new FindCommand(this.value, this.conn);
+                break;
+            case Command.DEL:
+                cmd = new DeleteCommand(this.key, this.conn);
                 break;
             default:
                 throw new Error("Unregistered Command");
