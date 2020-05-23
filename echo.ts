f6297f56bbe8ebec48f8fe18fd3f7064344891f4
Kyle Harrison <redactedprofile@gmail.com>
2020-05-23 16:10:50 -0700

    #10 quit command to allow graceful client disconnect, ungraceful client disconnect no longer crashes server

--- echo.ts
+++ echo.ts
@@ -5,6 +5,7 @@ import { Storage } from "./src/storage.ts";
 const env = Deno.env.toObject();
 const PORT = env.PORT || 8200;
 const HOST = env.HOST || "0.0.0.0";
+const MAX_CONNECTIONS = env.MAX_CONNECTIONS || 200;
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
@@ -28,7 +29,15 @@ async function handle_connection(connection: Deno.Conn) {
   let buffer = new Uint8Array(1024);
   let work = true;
   while (work) {
-    const count = await connection.read(buffer);
+    let count;
+    
+    try {
+      count = await connection.read(buffer);
+    } catch(ConnectionReset) {
+      // @todo clear connection from pool
+      work = false;
+    }
+
     if (!count) {
       // connection closed
       const index = connections.indexOf(connection);
@@ -37,9 +46,15 @@ async function handle_connection(connection: Deno.Conn) {
     } else {
       for (const current_connection of connections) {
         if (current_connection === connection) {
-          console.log("message received");
           let message = buffer.subarray(0, count);
           let query = decoder.decode(message).toString().trim();
+
+          if(query === 'quit') {
+            work = false;
+            await current_connection.write(encoder.encode(`Goodbye\r\n`));
+            await current_connection.close();
+            return;
+          }
 
           // console.log("Query received:", query);
           try {
