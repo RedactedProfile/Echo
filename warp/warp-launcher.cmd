7711c9a20a739081833b0e6bc3a960b314459830
Kyle Harrison <redactedprofile@gmail.com>
2020-05-30 16:57:35 -0700

    trying to get warp to work

--- warp/warp-launcher.cmd
+++ warp/warp-launcher.cmd
@@ -1,11 +1,5 @@
 @ECHO OFF
 
-SETLOCAL
+CALL deno.exe run --allow-net --allow-env .\echo.bundle.js 
 
-SET "APP_MAIN_JS=echo.bundle.js"
-
-SET "DENO_EXE=deno.exe"
-SET "APP_MAIN_JS_PATH=%APP_MAIN_JS%"
-
-CALL %DENO_EXE% run --allow-net --allow-env %APP_MAIN_JS_PATH% %*
 EXIT /B %ERRORLEVEL%
